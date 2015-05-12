8b72ee8fcd65dd90d7f047c1f8e3d5a6f35c9a09
Christian Alis <ianalis@gmail.com>
2015-05-12 17:18:02 +0100

    Add docstrings

--- treepy/client.py
+++ treepy/client.py
@@ -7,17 +7,43 @@ from operator import itemgetter
 from .utils import to_pandas_tseries
 
 class TreeClient(object):
+    """Client for interacting with the tree.mu API"""
     def __init__(self, platform_url='http://api.tree.mu'):
         self.platform_url = platform_url
         
-    def get_most_correlated(self, tseries, results_count=100, method='pearson', 
-                            freq='D'):
-        """Return most correlated articles"""
-        params = [('values', tseries.to_json()),
-                  ('results_count', results_count),
+    def get_most_correlated(self, title_or_tseries, results_count=50, 
+                            method='pearson', freq='D'):
+        """
+        Return most correlated articles
+        
+        
+        Parameters
+        ----------
+        title_or_tseries : str or pandas TimeSeries
+            Article title or time series to correlate with. The title must 
+            exactly match the Wikipedia title including case, underscores and 
+            escaped characters.
+        results_count : integer
+            Number of most correlated articles to return
+        method : "pearson", "spearman", "kendall"
+            Method of correlation
+        freq : "D", "W-SUN", "MS"
+            Frequency of time series
+            
+        
+        Returns
+        -------
+        results : list of dict
+            List of most correlated articles ordered by decreasing correlation
+            coefficient
+        """
+        params = [('results_count', results_count),
                   ('method', method),
                   ('freq', freq)]
-        print freq
+        if isinstance(title_or_tseries, basestring):
+            params.append(('title', title_or_tseries))
+        else:
+            params.append(('values', title_or_tseries.to_json()))
         
         results = json.load(urlopen(self.platform_url + '/corr/', 
                                     data=urlencode(params)))
@@ -28,7 +54,24 @@ class TreeClient(object):
         return results
     
     def get_page(self, title, freq='D'):
-        """Get page views time series"""
+        """
+        Get page views time series of a particular article
+        
+        
+        Parameters
+        ----------
+        title : str
+            Article title which should match the Wikipedia title including
+            case, underscores and escaped characters
+        freq :  "D", "W-SUN", "MS"
+            Frequency of time series
+            
+            
+        Returns
+        -------
+        tseries : pandas.TimeSeries
+            Pageviews normalized with total pageviews
+        """
         params = {'freq': freq}
         tseries = json.load(urlopen('%s/page/%s?%s' % (self.platform_url,
                                                      quote(title),
@@ -36,8 +79,22 @@ class TreeClient(object):
         return to_pandas_tseries(tseries)
     
     def get_total(self, freq='D'):
-        """Get total page views time series"""
+        """
+        Get the time series of total pageviews
+                
+        Parameters
+        ----------
+        freq :  "D", "W-SUN", "MS"
+            Frequency of time series
+            
+        
+        Returns
+        -------
+        tseries : pandas.TimeSeries
+            Time series of total pageviews
+        """
         params = {'freq': freq}
         tseries = json.load(urlopen('%s/page/?%s' % (self.platform_url,
                                                    urlencode(params))))
         return to_pandas_tseries(tseries)
+    
--- treepy/utils.py
+++ treepy/utils.py
@@ -1,13 +1,47 @@
 import pandas as pd
 
 def to_pandas_tseries(tseries):
-    """Convert tseries dict to pandas TimeSeries"""
+    """
+    Convert dict time series to a pandas TimeSeries
+    
+    tree.mu sends and receives time series values as a dict (json-encoded) of 
+    timestamp (in seconds) and value pairs. This function will convert it to a
+    pandas TimeSeries.
+    
+    
+    Parameters
+    ----------
+    tseries : dict 
+        dict of timestamp (in seconds) as key
+    
+    
+    Returns
+    -------
+    pd_timeseries : pandas.TimeSeries
+        Converted time series with timestamp as index   
+    """
     tseries = pd.TimeSeries(tseries)
     tseries.index = pd.to_datetime(tseries.index.astype(int), unit='s')
     return tseries
 
 def results_to_dframe(results):
-    """Convert results to a data frame"""
+    """
+    Convert results to a data frame
+    
+    Flattens correlation results into a data frame. Time series values are
+    excluded.
+    
+    
+    Parameters
+    ----------
+    results : list of dict
+        This is the output of TreeClient.get_most_correlated()
+        
+    Returns
+    -------
+    df : pandas.DataFrame
+        Data frame of results
+    """
     data = []
     for r in results:
         d = {
